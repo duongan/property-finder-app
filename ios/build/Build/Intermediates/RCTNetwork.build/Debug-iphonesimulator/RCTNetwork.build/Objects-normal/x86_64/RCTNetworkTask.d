c68ba87ef7c360bae803bb5d0cedc2eba15b67e6
An Duong <duongan1202@gmail.com>
2016-10-19 17:25:15 +0700

    commit 2

--- ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTNetworkTask.d
+++ ios/build/Build/Intermediates/RCTNetwork.build/Debug-iphonesimulator/RCTNetwork.build/Objects-normal/x86_64/RCTNetworkTask.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/an.duong/Documents/React-Native/PropertyFinder/node_modules/react-native/Libraries/Network/RCTNetworkTask.m \
-  /Users/an.duong/Documents/React-Native/PropertyFinder/node_modules/react-native/Libraries/Network/RCTNetworkTask.h \
+  /Users/an.duong/Documents/ReactNative/PropertyFinder/node_modules/react-native/Libraries/Network/RCTNetworkTask.m \
+  /Users/an.duong/Documents/ReactNative/PropertyFinder/node_modules/react-native/Libraries/Network/RCTNetworkTask.h \
   ../../React/Base/RCTURLRequestDelegate.h \
   ../../React/Base/RCTURLRequestHandler.h \
   ../../React/Base/RCTBridgeModule.h ../../React/Base/RCTDefines.h \
--- ios/build/Build/Intermediates/RCTWebSocket.build/Debug-iphonesimulator/RCTWebSocket.build/Objects-normal/x86_64/RCTSRWebSocket.d
+++ ios/build/Build/Intermediates/RCTWebSocket.build/Debug-iphonesimulator/RCTWebSocket.build/Objects-normal/x86_64/RCTSRWebSocket.d
@@ -1,5 +1,3 @@
 dependencies: \
-  /Users/an.duong/Documents/React-Native/PropertyFinder/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.m \
-  /Users/an.duong/Documents/React-Native/PropertyFinder/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.h \
-  ../../React/Base/RCTAssert.h ../../React/Base/RCTDefines.h \
-  ../../React/Base/RCTLog.h
+  /Users/an.duong/Documents/ReactNative/PropertyFinder/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.m \
+  /Users/an.duong/Documents/ReactNative/PropertyFinder/node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.h
